ad3e64d4b0af3e32b7356a7ce089a63c1f7e4a1c
EthanJWright <ewright362@gmail.com>
2021-11-28 20:15:23 -0700

    feat: create table from a txt file based on new lines

--- src/module/table.process.ts
+++ src/module/table.process.ts
@@ -5,31 +5,67 @@ export interface TableEntry {
   text: string;
 }
 
-export interface FoundryTableJSON {
+export interface FoundryTable {
   title: string;
-  strategy?: 'foundry';
   formula: string;
   entries: TableEntry[];
 }
 
-export interface BasicTableJSON {
+export interface BasicTable {
   title: string;
-  strategy?: 'basic';
-  entries: TableEntry[];
+  entries: string[];
 }
 
-export function parseBasicJSON({ title, entries }: BasicTableJSON) {
+export function isFoundryTable(table: FoundryTable | BasicTable) {
+  return (table as FoundryTable).formula !== undefined;
+}
+
+export function isBasicTable(table: FoundryTable | BasicTable) {
+  return (table as FoundryTable).formula === undefined;
+}
+
+const entryMap = (current: string, index: number): TableEntry => {
+  return {
+    text: current,
+    range: [index + 1, index + 1],
+  };
+};
+
+export function parseBasicJSON({ title, entries }: BasicTable) {
   return {
     name: title,
     formula: `1d${entries.length}`,
-    results: [...entries],
+    results: [...entries.map(entryMap)],
   };
 }
 
-export function parseFoundryJSON({ title, formula, entries }: FoundryTableJSON) {
+export function parseFoundryJSON({ title, formula, entries }: FoundryTable) {
   return {
     name: title,
     formula,
     results: [...entries],
+  };
+}
+
+function nameFromFile(file: string) {
+  // get the filename without the extension
+  const withPath = file.split('.')[0];
+  // remove the file path
+  const name = withPath.split('/').pop() || withPath;
+  // replace all underscores with spaces
+  // and capitalize the first letter of each word
+  return name
+    .replace(/_/g, ' ')
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+export function parseFromTxt(table: BasicTable) {
+  const { title, entries } = table;
+  return {
+    name: nameFromFile(title),
+    formula: `1d${entries.length}`,
+    results: [...entries.map(entryMap)],
   };
 }
--- src/module/table.ts
+++ src/module/table.ts
@@ -1,22 +1,39 @@
-import { FoundryTableJSON, parseFoundryJSON, TableData } from './table.process';
+import {
+  BasicTable,
+  FoundryTable,
+  isFoundryTable,
+  parseBasicJSON,
+  parseFoundryJSON,
+  parseFromTxt,
+  TableData,
+} from './table.process';
 
-async function createTableFromJSON(tableJSON: FoundryTableJSON) {
+async function createTableFromJSON(tableJSON: FoundryTable | BasicTable) {
   console.log(`creating a table...`);
-  const strategy = tableJSON.strategy || 'foundry';
   let parsed: TableData | undefined;
-  switch (strategy) {
-    case 'foundry':
-      parsed = parseFoundryJSON(tableJSON);
-      break;
-    default:
-      throw new Error(`unknown strategy: ${strategy}`);
+  if (isFoundryTable(tableJSON)) {
+    parsed = parseFoundryJSON(tableJSON as FoundryTable);
+  } else {
+    parsed = parseBasicJSON(tableJSON as BasicTable);
   }
   await RollTable.create(parsed);
 }
 
 async function jsonRoute(stringData: string) {
-  const json = JSON.parse(stringData) as FoundryTableJSON;
+  const json = JSON.parse(stringData) as FoundryTable;
   createTableFromJSON(json);
+}
+
+async function txtRoute(fullFileName: string, stringData: string) {
+  console.log(`Data: ${stringData}`);
+  const rawLines = stringData.split(/\r?\n/);
+  console.log(`raw lines: ${rawLines}`);
+  const lines = rawLines.filter((line) => {
+    return line !== '';
+  });
+  console.log(`lines: ${lines}`);
+  const parsed = parseFromTxt({ title: fullFileName, entries: lines });
+  await RollTable.create(parsed);
 }
 
 export async function processTableJSON(fullFileName: string) {
@@ -30,6 +47,9 @@ export async function processTableJSON(fullFileName: string) {
   switch (ext) {
     case 'json':
       jsonRoute(data);
+      break;
+    case 'txt':
+      txtRoute(fullFileName, data);
       break;
     default:
       console.log(`Unknown file type ${ext}`);
