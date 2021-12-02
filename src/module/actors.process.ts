3b9437aef03042efa8bccba75978875b5f27de82
EthanJWright <ewright362@gmail.com>
2021-12-01 21:57:52 -0700

    feat: extract stats

--- src/module/actors.process.ts
+++ src/module/actors.process.ts
@@ -86,12 +86,12 @@ export interface ImportActor {
     max: number;
   };
   armorClass: ArmorClass;
+  stats: Abilities;
 }
 
 export function parseHealth(line: string) {
   // line: Hit Points 66 (12d8 + 12)
   // get string from between parentheses
-  console.log(`line: ${line}`);
   // match = (12d8 + 12),12d8 + 12
   const formulaArray = line.match(/\(([^)]+)\)/);
   if (!formulaArray || formulaArray.length < 2) {
@@ -145,6 +145,81 @@ export function parseAC(acString: string): ArmorClass {
   };
 }
 
+interface Ability {
+  value: number;
+  mod: number;
+}
+
+interface Abilities {
+  str: Ability;
+  dex: Ability;
+  con: Ability;
+  int: Ability;
+  wis: Ability;
+  cha: Ability;
+}
+
+function parseAbilityScore(score: number, mod: string): Ability {
+  // score: 12
+  // mod: (+2)
+  // get string from between parentheses
+  const modNumberArray = mod.match(/\d+/);
+  if (!modNumberArray || modNumberArray.length < 1) {
+    throw new Error(`Could not parse ability score from string: ${mod}`);
+  }
+  let modNumber = modNumberArray[0];
+  if (mod.includes('-')) {
+    modNumber = '-' + modNumber;
+  }
+
+  return {
+    value: score,
+    mod: Number(modNumber),
+  };
+}
+
+export function parseStats(inputList: string[]) {
+  const abilityLine = inputList.find((line) => line.toUpperCase().includes('CHA'));
+  if (!abilityLine) {
+    throw new Error('Could not find ability line');
+  }
+  const abilityIndex = inputList.indexOf(abilityLine);
+  const singleLine = abilityLine.includes('STR');
+  if (singleLine) {
+    // match 3 to 6 letters
+    const abilityKeys = abilityLine.match(/\w{3,7}/g);
+    if (!abilityKeys || abilityKeys.length < 6) {
+      throw new Error('Could not find ability keys');
+    }
+    const valueLine = inputList[abilityIndex + 1];
+    // match 1 to 2 numbers
+    const abilityValuesWithMods = valueLine.match(/\d{1,2}/g);
+    if (!abilityValuesWithMods || abilityValuesWithMods.length < 6) {
+      throw new Error('Could not find ability values');
+    }
+    const abilityValues: number[] = [];
+    abilityValuesWithMods.forEach((value, index) => {
+      if (index % 2 === 0) {
+        abilityValues.push(Number(value));
+      }
+    });
+    // match + numbers in parentheses
+    const abilityModifiers = valueLine.match(/\(([+-]\d+)\)/g);
+    if (!abilityModifiers || abilityModifiers.length < 6) {
+      throw new Error('Could not find ability modifiers');
+    }
+    if (!abilityValues || abilityValues.length < 6) {
+      throw new Error('Could not find ability values');
+    }
+    const zipped = abilityKeys.reduce(
+      (obj, k, i) => ({ ...obj, [k.toLowerCase()]: parseAbilityScore(abilityValues[i], abilityModifiers[i]) }),
+      {},
+    );
+    return zipped as Abilities;
+  }
+  throw new Error('Could not parse ability line');
+}
+
 export function textToActor(input: string): ImportActor {
   const lines = input.split('\n');
   const healthLine = lines.find((line) => line.includes('Hit Points')) || '(1d6 + 1)';
@@ -157,5 +232,6 @@ export function textToActor(input: string): ImportActor {
     biography: lines[1].trim(),
     health: parseHealth(healthLine),
     armorClass: parseAC(acLine),
+    stats: parseStats(lines),
   };
 }
--- test/actors.process.test.ts
+++ test/actors.process.test.ts
@@ -1,4 +1,4 @@
-import { parseHealth, textToActor } from '../src/module/actors.process';
+import { parseStats, parseHealth, textToActor } from '../src/module/actors.process';
 describe('parseHealth', () => {
   it('should parse a valid health string', () => {
     const health = parseHealth('Hit Points 66 (12d8 + 12)');
@@ -8,10 +8,44 @@ describe('parseHealth', () => {
   });
 });
 
+describe('extractStats', () => {
+  it('should extract stats from a valid string', () => {
+    const actorText =
+      'Swashbuckler\nMedium humanoid (any race), any non-lawful alignment\n\nArmor Class 17 (leather armor)\nHit Points 66 (12d8 + 12)\nSpeed 30 ft.\n\n   STR        DEX         CON        INT        WIS         CHA\n  12 (+1)    18 (+4)     12 (+1)    14 (+2)    11 (+0)     15 (+2)\n\nSkills Acrobatics +8, Athletics +5, Persuasion +6\nSenses passive Perception 10\nLanguages any one language (usually Common)\nChallenge 3 (700 XP)\n\nLightfooted. The swashbuckler can take the Dash or Disengage\naction as a bonus action on each of its turns.\n\nSuave Defense. While the swashbuckler is wearing light or no\narmor and wielding no shield, its AC includes its Charisma\nmod.\n\nActions\n\nMultiattack. The swashbuckler makes three attacks: one with\na dagger and two with its rapier.\nDagger. Melee or Ranged Weapon Attack: +6 to hit, reach 5\nft. or range 20/60 ft., one target. Hit: 6 (1d4 + 4) piercing\ndamage.\nRapier. Melee Weapon Attack: +6 to hit, reach 5 ft., one target.\nHit: 8 (1d8 + 4) piercing damage.';
+    const stats = parseStats(actorText.split('\n'));
+    expect(stats).toEqual({
+      str: {
+        value: 12,
+        mod: 1,
+      },
+      dex: {
+        value: 18,
+        mod: 4,
+      },
+      con: {
+        value: 12,
+        mod: 1,
+      },
+      int: {
+        value: 14,
+        mod: 2,
+      },
+      wis: {
+        value: 11,
+        mod: 0,
+      },
+      cha: {
+        value: 15,
+        mod: 2,
+      },
+    });
+  });
+});
+
 describe('Parse Text', () => {
   it('should parse the text into an actor', () => {
     const actorText =
-      'Swashbuckler\nMedium humanoid (any race), any non-lawful alignment\n\nArmor Class 17 (leather armor)\nHit Points 66 (12d8 + 12)\nSpeed 30 ft.\n\n   STR        DEX         CON        INT        WIS         CHA\n  12 (+1)    18 (+4)     12 (+1)    14 (+2)    11 (+0)     15 (+2)\n\nSkills Acrobatics +8, Athletics +5, Persuasion +6\nSenses passive Perception 10\nLanguages any one language (usually Common)\nChallenge 3 (700 XP)\n\nLightfooted. The swashbuckler can take the Dash or Disengage\naction as a bonus action on each of its turns.\n\nSuave Defense. While the swashbuckler is wearing light or no\narmor and wielding no shield, its AC includes its Charisma\nmodifier.\n\nActions\n\nMultiattack. The swashbuckler makes three attacks: one with\na dagger and two with its rapier.\nDagger. Melee or Ranged Weapon Attack: +6 to hit, reach 5\nft. or range 20/60 ft., one target. Hit: 6 (1d4 + 4) piercing\ndamage.\nRapier. Melee Weapon Attack: +6 to hit, reach 5 ft., one target.\nHit: 8 (1d8 + 4) piercing damage.';
+      'Swashbuckler\nMedium humanoid (any race), any non-lawful alignment\n\nArmor Class 17 (leather armor)\nHit Points 66 (12d8 + 12)\nSpeed 30 ft.\n\n   STR        DEX         CON        INT        WIS         CHA\n  12 (+1)    18 (+4)     12 (+1)    14 (+2)    11 (+0)     15 (+2)\n\nSkills Acrobatics +8, Athletics +5, Persuasion +6\nSenses passive Perception 10\nLanguages any one language (usually Common)\nChallenge 3 (700 XP)\n\nLightfooted. The swashbuckler can take the Dash or Disengage\naction as a bonus action on each of its turns.\n\nSuave Defense. While the swashbuckler is wearing light or no\narmor and wielding no shield, its AC includes its Charisma\nmod.\n\nActions\n\nMultiattack. The swashbuckler makes three attacks: one with\na dagger and two with its rapier.\nDagger. Melee or Ranged Weapon Attack: +6 to hit, reach 5\nft. or range 20/60 ft., one target. Hit: 6 (1d4 + 4) piercing\ndamage.\nRapier. Melee Weapon Attack: +6 to hit, reach 5 ft., one target.\nHit: 8 (1d8 + 4) piercing damage.';
     const actor = textToActor(actorText);
     expect(actor.name).toBe('Swashbuckler');
     expect(actor.biography).toBe('Medium humanoid (any race), any non-lawful alignment');
