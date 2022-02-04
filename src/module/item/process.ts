e95dbdac78718b8e3a4b6a1ad95cf82e9bb2d599
EthanJWright <ewright362@gmail.com>
2022-02-03 21:12:59 -0700

    feat: add item rarity parsing

--- src/module/item/process.ts
+++ src/module/item/process.ts
@@ -1,12 +1,17 @@
+type ItemRarity = 'common' | 'uncommon' | 'rare' | 'legendary';
+
 interface ItemData {
   description: {
     value: string;
   };
+  rarity: ItemRarity;
 }
+
+type ItemType = 'weapon' | 'equipment' | 'consumable' | 'tool' | 'loot' | 'class' | 'spell' | 'feat' | 'backpack';
 
 export interface Item {
   name: string;
-  type: string;
+  type: ItemType;
   data: ItemData;
 }
 
@@ -14,14 +19,28 @@ function parseName(input: string): string {
   return input.split('\n')[0].trim();
 }
 
+function parseType(input: string): ItemType {
+  const type = 'consumable';
+  return type;
+}
+
+function parseRarity(input: string): ItemRarity {
+  if (/uncommon/i.test(input)) return 'uncommon';
+  if (/common/i.test(input)) return 'common';
+  if (/rare/i.test(input)) return 'rare';
+  if (/legendary/i.test(input)) return 'legendary';
+  return 'common';
+}
+
 export function processItem(input: string): Item {
   return {
     name: parseName(input),
-    type: 'item',
+    type: parseType(input),
     data: {
       description: {
         value: '',
       },
+      rarity: parseRarity(input),
     },
   };
 }
--- test/item.process.test.ts
+++ test/item.process.test.ts
@@ -6,5 +6,7 @@ describe('processItem', () => {
       'Figurine of Wondrous Power - Jingles\nWondrous item, uncommon\nThis figurine of a black horse can become a horse\nwearing a white saddle adorned with jingle bells,\nwhich cannot be removed. Once it has been used, it\ncan’t be used again until the following dawn. While in\nhorse form, the figurine allows you to cast the animal\nmessenger spell on it at will.\nFor more information, please refer to the 5th edition\nDungeon Master’s Guide.';
     const parsed = processItem(itemText);
     expect(parsed.name).toBe('Figurine of Wondrous Power - Jingles');
+    expect(parsed.type).toBe('consumable');
+    expect(parsed.data.rarity).toBe('uncommon');
   });
 });
