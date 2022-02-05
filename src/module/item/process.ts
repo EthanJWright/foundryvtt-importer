b1d87cbab88acd7278d22d454abc04bd2686f76d
EthanJWright <ewright362@gmail.com>
2022-02-04 23:32:08 -0700

    fix: cleaning how description is generated

--- src/module/item/process.ts
+++ src/module/item/process.ts
@@ -26,15 +26,24 @@ function parseRarity(input: string): ItemRarity {
   return 'common';
 }
 
-function scrubDescription(description: string, extraItems: string[]): string {
-  return extraItems.reduce((acc, item) => acc.replace(item, '').trim(), description).replace('\n', '');
+function cleanDescription(description: string, extraItems: string[]): string {
+  return (
+    extraItems
+      .reduce((acc, item) => {
+        const afterItem = acc.split(item)[1] || acc;
+        return afterItem;
+      }, description)
+      .replace(/(\r\n|\n|\r)/gm, ' ')
+      .trim() || description
+  );
 }
 
 export function processItem(input: string): FifthItem {
   const type = parseType(input);
   const name = parseName(input);
+  const rarity = parseRarity(input);
   // TODO: fix issue where type and rarity and such are still in the description
-  const description = scrubDescription(input, [name, type]);
+  const description = cleanDescription(input, [name, type, rarity]);
   switch (type) {
     case 'weapon':
     case 'feat':
@@ -47,7 +56,7 @@ export function processItem(input: string): FifthItem {
           description: {
             value: description,
           },
-          rarity: parseRarity(input),
+          rarity,
         },
       };
   }
--- test/item.process.test.ts
+++ test/item.process.test.ts
@@ -8,5 +8,8 @@ describe('processItem', () => {
     expect(parsed.name).toBe('Figurine of Wondrous Power - Jingles');
     expect(parsed.type).toBe('consumable');
     expect(parsed.data.rarity).toBe('uncommon');
+    expect(parsed.data?.description?.value).toBe(
+      'This figurine of a black horse can become a horse wearing a white saddle adorned with jingle bells, which cannot be removed. Once it has been used, it can’t be used again until the following dawn. While in horse form, the figurine allows you to cast the animal messenger spell on it at will. For more information, please refer to the 5th edition Dungeon Master’s Guide.',
+    );
   });
 });
