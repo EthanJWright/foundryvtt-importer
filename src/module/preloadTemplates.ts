b4d123ee66efd4b215fc2b9f89b3215723cfde74
EthanJWright <ewright362@gmail.com>
2022-02-05 10:22:21 -0700

    fix: preload file based on CONSTANTS

--- src/module/preloadTemplates.ts
+++ src/module/preloadTemplates.ts
@@ -1,8 +1,10 @@
+import CONSTANTS from './constants';
+
 export async function preloadTemplates(): Promise<Handlebars.TemplateDelegate[]> {
   const templatePaths: string[] = [
     // Add paths to "modules/foundryvtt-importer/templates"
-    'modules/foundryvtt-importer/templates/importForm.hbs',
-    'modules/foundryvtt-importer/templates/importTableForm.hbs',
+    `modules/${CONSTANTS.module.name}/templates/importForm.hbs`,
+    `modules/${CONSTANTS.module.name}/templates/importTableForm.hbs`,
   ];
 
   return loadTemplates(templatePaths);
