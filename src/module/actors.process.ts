ef76773ad5ec9f048501d58c5ba60dc21b7276ac
EthanJWright <ewright362@gmail.com>
2022-02-05 00:24:40 -0700

    fix: removing deprecated method

--- src/module/actors.process.ts
+++ src/module/actors.process.ts
@@ -780,7 +780,7 @@ function getType(lines: string[]): string {
 
 function capitalizeBeginings(str: string): string {
   return str.replace(/\w\S*/g, function (txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
   });
 }
 
