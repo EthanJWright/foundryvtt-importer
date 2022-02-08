2d44424206d8a614a81b343d1f05c20de875186a
EthanJWright <ewright362@gmail.com>
2022-02-07 20:21:13 -0700

    refactor: pulling available parser lists into separate location

---
+++ src/module/actor/parsers/available.ts
@@ -0,0 +1,41 @@
+import {
+  parseNameWTC,
+  parseACWTC,
+  parseAlignmentWTC,
+  parseBiographyWTC,
+  parseConditionImmunitiesWTC,
+  parseDamageImmunitiesWTC,
+  parseDamageResistancesWTC,
+  parseDamageVulnerabilitiesWTC,
+  parseFeaturesWTC,
+  parseHealthWTC,
+  parseLanguagesWTC,
+  parseMultilineStats,
+  parseRatingWTC,
+  parseSensesWTC,
+  parseSizeWTC,
+  parseSkillsWTC,
+  parseSpeedWTC,
+  parseStatsWTC,
+  parseTypeWTC,
+  parseVerticalKeyValueStats,
+} from './wtcTextBlock';
+
+export const nameParsers = [parseNameWTC];
+export const ratingParsers = [parseRatingWTC];
+export const typeParsers = [parseTypeWTC];
+export const alignmentParsers = [parseAlignmentWTC];
+export const biographyParsers = [parseBiographyWTC];
+export const languagesParsers = [parseLanguagesWTC];
+export const sizeParsers = [parseSizeWTC];
+export const healthParsers = [parseHealthWTC];
+export const sensesParsers = [parseSensesWTC];
+export const acParsers = [parseACWTC];
+export const damageImmunitiesParsers = [parseDamageImmunitiesWTC];
+export const damageResistancesParsers = [parseDamageResistancesWTC];
+export const conditionImmunitiesParsers = [parseConditionImmunitiesWTC];
+export const damageVulnerabilitiesParsers = [parseDamageVulnerabilitiesWTC];
+export const statsParsers = [parseStatsWTC, parseMultilineStats, parseVerticalKeyValueStats];
+export const speedParsers = [parseSpeedWTC];
+export const skillsParsers = [parseSkillsWTC];
+export const featuresParsers = [parseFeaturesWTC];
