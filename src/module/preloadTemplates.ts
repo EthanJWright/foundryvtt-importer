8a2bd985c3fedf485d8f28f7337d44da30c7c8e1
EthanJWright <ewright362@gmail.com>
2022-06-15 09:10:36 -0600

    fixing preload templates

--- src/module/preloadTemplates.ts
+++ src/module/preloadTemplates.ts
@@ -3,8 +3,10 @@ import CONSTANTS from './constants';
 export async function preloadTemplates(): Promise<Handlebars.TemplateDelegate[]> {
   const templatePaths: string[] = [
     // Add paths to "modules/foundryvtt-importer/templates"
-    `modules/${CONSTANTS.module.name}/templates/importForm.hbs`,
     `modules/${CONSTANTS.module.name}/templates/importTableForm.hbs`,
+    `modules/${CONSTANTS.module.name}/templates/importActorForm.hbs`,
+    `modules/${CONSTANTS.module.name}/templates/importItemForm.hbs`,
+    `modules/${CONSTANTS.module.name}/templates/importJournalForm.hbs`,
   ];
 
   return loadTemplates(templatePaths);
