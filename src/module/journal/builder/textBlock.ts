aea04fafdd3af1a0dbbae86a10734933eb98011b
EthanJWright <ewright362@gmail.com>
2022-06-17 19:25:31 -0600

    remove old console log

--- src/module/journal/builder/textBlock.ts
+++ src/module/journal/builder/textBlock.ts
@@ -6,7 +6,6 @@ export interface JournalOptions {
 }
 
 export async function buildTextBlock(input: TextBlock, api: FoundryApi, opts: JournalOptions): Promise<void> {
-  console.log(`Creating: ${JSON.stringify(input, null, 2)}`);
   await api.foundryJournalEntry.create({
     name: input?.name ?? 'Unknown Name',
     content: input.content,
