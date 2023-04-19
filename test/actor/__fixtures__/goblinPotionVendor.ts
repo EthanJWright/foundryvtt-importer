70cd7ccd894ffa11eebfc4a25f803f51ec0e1761
Ethan James Wright <ewright362@gmail.com>
2023-04-19 00:34:50 -0600

    chore(actor.test): Extract fixture strings into new files (#96)
    * fix(module.json): version compatability matrix
    
    * remove system from module
    
    * remove empty dependencies
    
    * pull out all fixtures

---
+++ test/actor/__fixtures__/goblinPotionVendor.ts
@@ -0,0 +1,2 @@
+export const GOBLIN_POTION_VENDOR =
+  'Goblin Potion Vendor\n\nSmall humanoid (goblin), neutral\n\nArmor Class 12\nHit Points 6 (2d6)\nSpeed 30 ft.\n\nSTR 8 (-1) DEX 14 (+2) CON 10 (+0) INT 12 (+1) WIS 10 (+0) CHA 8 (-1)\n\nSkills: Arcana +3, Deception +1, Perception +2\nSenses: darkvision 60 ft., passive Perception 12\nLanguages: Common, Goblin\n\nActions:\n- Scimitar: Melee Weapon Attack: +4 to hit, reach 5 ft., one target. Hit: 5 (1d6 + 2) slashing damage.\n- Dart: Ranged Weapon Attack: +4 to hit, range 20/60 ft., one target. Hit: 4 (1d4 + 2) piercing damage.\n';
